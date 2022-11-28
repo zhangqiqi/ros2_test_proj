72491fd38073d2fd6c9620e6de019739bc712bb8
zhangqiqi <https://github.com/zhangqiqi/ros2_test_proj.git>
2022-11-29 00:34:29 +0800

    add worlds dir and base world model;now load model to gazebo can work, and it's camera can use by rqt image display function

--- src/tank_model/launch/display_tank_model.launch.py
+++ src/tank_model/launch/display_tank_model.launch.py
@@ -62,6 +62,6 @@ def generate_launch_description():
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node
+        # rviz_node,
         # test_node
     ])
